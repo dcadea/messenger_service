2cc921418e29e773b6949714e9d00ebc8e38874a
dcadea <dmitrii.cadea@gmail.com>
2025-02-09 14:12:49 +0200

    cleanup user markup

--- src/user/markup.rs
+++ src/user/markup.rs
@@ -9,14 +9,12 @@ pub struct Header<'a>(pub &'a UserInfo);
 impl Render for Header<'_> {
     fn render(&self) -> Markup {
         html! {
-            header id="user-header"
-                class="flex justify-between items-center mb-4"
-            {
-                img class="w-12 h-12 rounded-full mr-2"
+            header #user-header ."flex justify-between items-center mb-4" {
+                img ."w-12 h-12 rounded-full mr-2"
                     src=(self.0.picture)
                     alt="User avatar" {}
-                h2.text-2xl {(self.0.name)}
-                a class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                h2 .text-2xl {(self.0.name)}
+                a ."bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
                     href="/logout" { "Logout" }
             }
         }
@@ -28,8 +26,7 @@ pub struct Search;
 impl Render for Search {
     fn render(&self) -> Markup {
         html! {
-            input
-                class="mb-4 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
+            input ."mb-4 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
                 type="search"
                 name="nickname"
                 placeholder="Search users..."
@@ -43,7 +40,7 @@ impl Render for Search {
                             remove children of #search-results
                     "# {}
 
-            div id="search-results" class="relative" {}
+            div #search-results .relative {}
         }
     }
 }
@@ -53,14 +50,14 @@ struct AddFriend<'a>(&'a Sub);
 impl Render for AddFriend<'_> {
     fn render(&self) -> Markup {
         html! {
-            form class="float-right"
+            form .float-right
                 hx-post="/api/chats"
                 hx-target="#chat-window"
             {
                 input type="hidden" name="sub" value=(self.0) {}
-                input type="submit"
-                    value="Add friend"
-                    class="px-2 py-1 text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-xs focus:outline-none" {}
+                input ."px-2 py-1 text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-xs focus:outline-none"
+                    type="submit"
+                    value="Add friend" {}
             }
         }
     }
@@ -83,18 +80,16 @@ impl Render for SearchResult<'_> {
             "absolute w-full bg-white border border-gray-300 rounded-md shadow-lg";
 
         html! {
-            @if self.users.is_empty() {
-                ul class=({search_result_class}) {
-                    li class="px-3 py-2" { "No users found" }
-                }
-            } @else {
-                ul class=({search_result_class}) {
+            ul .(search_result_class) {
+                @if self.users.is_empty() {
+                    li ."px-3 py-2" { "No users found" }
+                } @else {
                     @for user in self.users {
-                        li class="px-3 py-2" {
-                            img class="w-6 h-6 rounded-full float-left"
+                        li ."px-3 py-2" {
+                            img ."w-6 h-6 rounded-full float-left"
                                 src=(user.picture)
                                 alt="User avatar" {}
-                            strong.px-3 {(user.name)} (user.nickname)
+                            strong .px-3 {(user.name)} (user.nickname)
 
                             @if !self.friends.contains(&user.sub) {
                                 (AddFriend(&user.sub))
